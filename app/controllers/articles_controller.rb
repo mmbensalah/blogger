5d04e5729ae605a875b37256379882199a53bd3b
mmbensalah <mmbensalah@gmail.com>
2018-08-25 22:06:42 -0600

    TESTS and IMPLEMENTS edit functionality for article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,6 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:destroy, :edit, :update]
+
   def index
     @articles = Article.all
   end
@@ -18,12 +20,23 @@ class ArticlesController < ApplicationController
     redirect_to article_path(@article)
   end
 
+  def edit
+  end
+
+  def update
+    @article.update(article_params)
+    redirect_to article_path(@article)
+  end
+
   def destroy
-    Article.destroy(params[:id])
+    @article.destroy
     redirect_to articles_path
   end
 
 private
+  def set_article
+      @article = Article.find(params[:id])
+  end
 
   def article_params
     params.require(:article).permit(:title, :body)
